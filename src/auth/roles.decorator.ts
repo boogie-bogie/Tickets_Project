96cf0b373e14c2b32e86aec0e7c84767c33dd8a7
boogie.bogiegie <boogie.bogiegie@gmail.com>
2024-03-11 16:07:21 +0900

    add: custom decorator, roles guard

---
+++ src/auth/roles.decorator.ts
@@ -0,0 +1,6 @@
+import { Role } from 'src/users/types/usersRole.type';
+
+import { SetMetadata } from '@nestjs/common';
+
+export const Roles = (...roles: Role[]) => 
+    SetMetadata('roles', roles); 
